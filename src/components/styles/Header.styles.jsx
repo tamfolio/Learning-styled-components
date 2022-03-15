a8e85b4420ac991ace9fc6cc848b4a7c37aa6dcf
unknown <tamiloreolaniyi@gmail.com>
2022-03-15 21:11:55 +0100

    adjusting mobile

--- src/components/styles/Header.styles.jsx
+++ src/components/styles/Header.styles.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 export const StyledHeader = styled.header `
     background-color: ${({ theme }) => theme.colors.header};
     padding: 40px 0;
-    height: 70vh;
 
     h1{
         color: black;
