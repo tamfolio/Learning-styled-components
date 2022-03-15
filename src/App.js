986235c7d70f67f0e7dcaa95c1f19884db915496
unknown <tamiloreolaniyi@gmail.com>
2022-03-15 17:31:12 +0100

    building web app with styled component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import {Container} from './components/styles/Container.styled'
+import Header from './components/Header';
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './components/styles/Global';
 
 
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+  mobile: '768px'
+}
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+    <>
+    <GlobalStyles/>
+    <Header/>
     <Container>
      <h1>Styled Components</h1>
     </Container>
+    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StyledHeader, Nav , Logo, Image} from './styles/Header.styles'
+import { Container } from './styles/Container.styled'
+import { Button } from './styles/Button.styled'
+import { Flex } from './styles/Flex.styles'
+
+export default function Header() {
+  return (
+    <StyledHeader >
+      <Container>
+        <Nav>
+          <Logo src='./images/logo.svg'></Logo>
+          <Button>Try it  Free</Button>
+        </Nav>
+        <Flex>
+          <div>
+            <h1>Build The Community Your Fans Will Love</h1>
+            <p> Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
+            <Button bg='#ff0099' color='#fff'>Get Started for Free</Button>
+          </div>
+          <Image src='./images/screen-mockups.svg' alt=''></Image>
+        </Flex>
+      </Container>
+    </StyledHeader> 
+  )
+}
